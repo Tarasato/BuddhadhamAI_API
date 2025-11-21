02a938e77a8d93ecd9d7d8dd09aa4e79ca9af3e1
Tarasato <taramiratsu.xato@gmail.com>
2025-11-21 15:55:56 +0700

    Add limit file

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 const server = http.createServer(app);
 const allowedOrigins = process.env.ORIGIN?.split(',')
 const io = new Server(server, {
